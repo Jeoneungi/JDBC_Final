cc42a208f140abb22aaa00d6b98692ee10742c4b
Jeoneungi <jeg3339@naver.com>
2023-06-04 14:23:58 +0900

    로그인 기능 구현

--- src/main/java/com/sbs/exam/board/repository/MemberRepository.java
+++ src/main/java/com/sbs/exam/board/repository/MemberRepository.java
@@ -1,18 +1,22 @@
 package com.sbs.exam.board.repository;
-
+import com.sbs.exam.board.Article;
+import com.sbs.exam.board.Member;
 import com.sbs.exam.board.util.DBUtil;
 import com.sbs.exam.board.util.SecSql;
 
 import java.sql.Connection;
+import java.util.Map;
 
 public class MemberRepository {
   private Connection conn;
+
   public MemberRepository(Connection conn) {
     this.conn = conn;
   }
 
   public boolean isLoginIdDup(String loginId) {
     SecSql sql = new SecSql();
+
     sql.append("SELECT COUNT(*) > 0");
     sql.append("FROM `member`");
     sql.append("WHERE loginId = ?", loginId);
@@ -32,4 +36,22 @@ public class MemberRepository {
 
     return DBUtil.insert(conn, sql);
   }
+
+  public Member getMemberByLoginId(String loginId) {
+    SecSql sql = new SecSql();
+
+    sql.append("SELECT *");
+    sql.append("FROM `member`");
+    sql.append("WHERE loginId = ?", loginId);
+
+    Map<String, Object> memberMap = DBUtil.selectRow(conn, sql);
+
+    if(memberMap.isEmpty()) {
+      return null;
+    }
+
+    return new Member(memberMap);
+  }
 }
+
+
--- src/main/java/com/sbs/exam/board/service/MemberService.java
+++ src/main/java/com/sbs/exam/board/service/MemberService.java
@@ -1,11 +1,10 @@
 package com.sbs.exam.board.service;
 
+import com.sbs.exam.board.Member;
 import com.sbs.exam.board.repository.MemberRepository;
 import com.sbs.exam.board.util.DBUtil;
 import com.sbs.exam.board.util.SecSql;
-
 import java.sql.Connection;
-
 public class MemberService {
   private MemberRepository memberRepository;
   public MemberService(Connection conn) {
@@ -14,8 +13,11 @@ public class MemberService {
   public boolean isLoginIdDup(String loginId) {
     return memberRepository.isLoginIdDup(loginId);
   }
-
   public int join(String loginId, String loginPw, String name) {
     return memberRepository.join(loginId, loginPw, name);
+  }
+
+  public Member getMemberByLoginId(String loginId) {
+    return memberRepository.getMemberByLoginId(loginId);
   }
 }
